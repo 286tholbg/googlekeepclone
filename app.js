92f71b47a42646bfa4f9dd61a7b7e762818d3746
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-10-30 23:17:12 +1100

    display notes function added

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ class App {
         this.notes = []
 
         this.$form = document.querySelector('#form')
+        this.$notes = document.querySelector('#notes')
+        this.$placeholder = document.querySelector('#placeholder-text')
         this.$noteTitle = document.querySelector('#note-title')
         this.$noteText = document.querySelector('#note-text')
         this.$formButtons = document.querySelector('#form-buttons')
@@ -26,6 +28,7 @@ class App {
 
             if(hasNote){
                 this.addNote({ title, text })
+                this.displayNotes()
             }
         })
     }
@@ -49,6 +52,8 @@ class App {
         this.$form.classList.add('form-open')
         this.$noteTitle.style.display = 'none';
         this.$formButtons.style.display = 'none';
+        this.$noteTitle.value = ''
+        this.$noteText.value = ''
     }
 
     addNote(note){
@@ -59,7 +64,27 @@ class App {
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id+1 : 1
         };
         this.notes = [...this.notes, newNote]
-        console.log(this.notes)
+        this.displayNotes()
+        this.closeForm()
+    }
+
+    displayNotes() {
+        const hasNotes = this.notes.length > 0
+        hasNotes ? this.$placeholder.style.display = 'none' : 'flex'
+
+        this.$notes.innerHTML = this.notes.map(note => (
+            `<div style="background: ${note.color}"; class="note">
+                <div class="${note.title && 'note-title'}">${note.title}</div>
+                <div class="note-text">${note.text}</div>
+                <div class="toolbar-container">
+                    <div class="toolbar">
+                        <span class="material-symbols-outlined toolbar-color">pan_tool_alt</span>
+                        <span class="material-symbols-outlined toolbar-delete">delete</span>
+                    </div>
+                </div>
+            </div>
+            `
+        )).join("")
     }
 }
 new App()
