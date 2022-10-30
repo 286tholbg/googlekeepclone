0027afbd082aed014fc7448f2a82189e061df82f
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-10-30 22:59:52 +1100

    added app.js add notes functionality

---
+++ app.js
@@ -0,0 +1,66 @@
+class App {
+    constructor() {
+        console.log('The app is working!')
+
+        this.notes = []
+
+        this.$form = document.querySelector('#form')
+        this.$noteTitle = document.querySelector('#note-title')
+        this.$noteText = document.querySelector('#note-text')
+        this.$formButtons = document.querySelector('#form-buttons')
+
+        this.addEventListeners()
+    }
+
+    addEventListeners() {
+        document.body.addEventListener('click', event => {
+            this.handleFormClick(event)
+        })
+
+        this.$form.addEventListener('submit', event => {
+            event.preventDefault()
+            const title = this.$noteTitle.value;
+            const text = this.$noteText.value;
+
+            const hasNote = title || text;
+
+            if(hasNote){
+                this.addNote({ title, text })
+            }
+        })
+    }
+
+    handleFormClick(event) {
+        const isFormClicked = this.$form.contains(event.target)
+        if(isFormClicked){
+            this.openForm()
+        } else {
+           this.closeForm()
+        }
+    }
+
+    openForm() {
+        this.$form.classList.add('form-open')
+        this.$noteTitle.style.display = 'block';
+        this.$formButtons.style.display = 'block';
+    }
+
+    closeForm(){
+        this.$form.classList.add('form-open')
+        this.$noteTitle.style.display = 'none';
+        this.$formButtons.style.display = 'none';
+    }
+
+    addNote(note){
+        const newNote = {
+            title: note.title,
+            text: note.text,
+            color: 'white',
+            id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id+1 : 1
+        };
+        this.notes = [...this.notes, newNote]
+        console.log(this.notes)
+    }
+}
+new App()
+
